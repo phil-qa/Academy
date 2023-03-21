28e2d178fbee03583616862351c8974f698383e1
phil-qa <104486392+phil-qa@users.noreply.github.com>
2023-03-21 10:40:59 +0000

    Initial commit

---
+++ Tester.py
@@ -0,0 +1,41 @@
+from tests import *
+
+class Tester:   
+    def __init__(self):
+        self._tests = []  #list of questions at startup
+        self._load_tests() 
+        
+
+    def _load_tests(self):
+        for test in tests:
+            self._tests.append(_test(test))
+            
+        
+        
+        
+  
+    #things it can do
+    #form and run a question and answer
+    pass
+
+
+
+class _test:
+    #things it knows
+    def __init__(self, test):
+        self.test_number = test['test_number']
+        self.question = test['question']
+        self.parts = self._get_parts(test)
+        
+    def _get_parts(self, test):
+        parts = []
+        for value in test['parts'].values():
+            parts.append((value['question'], value['ans']))
+        return parts
+        
+        
+    #the answers
+    #Things it can do
+    #serialise from dictionary at startup
+    pass
+
---
+++ tests.py
@@ -0,0 +1,6 @@
+tests = [
+           {'test_number': '001',
+           'question': 'What are the following types',
+           'parts' : [{'question' : '10', 'ans': 'int'}, {'question' : "'10'", 'ans': 'str'}, {'question' : '11.1', 'ans': 'float'}, {'question' : 'True', 'ans': 'bool'}]
+           }
+         ]
